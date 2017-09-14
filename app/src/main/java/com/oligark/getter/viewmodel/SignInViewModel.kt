f24672c54788218c85662631865d544b7eb6e031
Martín Vásquez <pedromvasquezb@gmail.com>
2017-09-14 03:11:19 -0500

    Minor change in SignInViewModel

--- app/src/main/java/com/oligark/getter/viewmodel/SignInViewModel.kt
+++ app/src/main/java/com/oligark/getter/viewmodel/SignInViewModel.kt
@@ -1,17 +1,18 @@
 package com.oligark.getter.viewmodel
 
 import android.arch.lifecycle.ViewModel
+import com.firebase.ui.auth.AuthUI
 
 /**
  * Created by pmvb on 17-09-13.
  */
 class SignInViewModel : ViewModel() {
 
-    enum class SIGNIN_PROVIDER {
-        GOOGLE,
-        TWITTER,
-        FACEBOOK,
-        EMAIL
+    enum class SIGNIN_PROVIDER(val provider: String) {
+        GOOGLE(AuthUI.GOOGLE_PROVIDER),
+        TWITTER(AuthUI.TWITTER_PROVIDER),
+        FACEBOOK(AuthUI.FACEBOOK_PROVIDER),
+        EMAIL(AuthUI.EMAIL_PROVIDER)
     }
 
     fun doLogin(provider: SIGNIN_PROVIDER, user: String = "", pass: String = "") {
