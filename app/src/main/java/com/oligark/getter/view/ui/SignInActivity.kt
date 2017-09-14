fc825497060780832939d81d3da4878d13da25b3
Martín Vásquez <pedromvasquezb@gmail.com>
2017-09-14 03:12:12 -0500

    Add fb login without viewmodel

--- app/src/main/java/com/oligark/getter/view/ui/SignInActivity.kt
+++ app/src/main/java/com/oligark/getter/view/ui/SignInActivity.kt
@@ -2,10 +2,21 @@ package com.oligark.getter.view.ui
 
 import android.arch.lifecycle.LifecycleActivity
 import android.arch.lifecycle.ViewModelProviders
+import android.content.Intent
 import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.FacebookAuthProvider
+import com.google.firebase.auth.FirebaseAuth
 
 import com.oligark.getter.R
 import com.oligark.getter.databinding.ActivitySigninBinding
@@ -16,15 +27,62 @@ class SignInActivity : LifecycleActivity() {
     companion object {
         @JvmField
         val TAG = SignInActivity::class.java.simpleName
+        @JvmField
+        val AUTH_CREDENTIAL_KEY = "auth_credential"
     }
 
     private lateinit var binding: ActivitySigninBinding
-    private lateinit var viewModel: SignInViewModel
+//    private lateinit var viewModel: SignInViewModel
+    private var credential: AuthCredential? = null
+
+    private val mFbCallbackManager: CallbackManager = CallbackManager.Factory.create()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_signin)
-        viewModel = ViewModelProviders.of(this).get(SignInViewModel::class.java)
+//        viewModel = ViewModelProviders.of(this).get(SignInViewModel::class.java)
+        LoginManager.getInstance().registerCallback(
+                mFbCallbackManager,
+                object : FacebookCallback<LoginResult> {
+                    override fun onCancel() {
+                        Log.d(TAG, "Login cancelled")
+                    }
+                    override fun onSuccess(result: LoginResult) {
+                        Log.d(TAG, "Login success")
+                        handleFbLogin(result.accessToken)
+                    }
+                    override fun onError(error: FacebookException?) {
+                        Log.e(TAG, "Login error", error)
+                    }
+                }
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (mFbCallbackManager.onActivityResult(requestCode, resultCode, data)) {
+            launchMainActivity()
+            return
+        }
+    }
+
+    private fun launchMainActivity() {
+        val mainIntent = Intent(this, MainActivity::class.java)
+        mainIntent.putExtra(AUTH_CREDENTIAL_KEY, credential)
+        startActivity(mainIntent)
+        finish()
+    }
+
+    private fun handleFbLogin(accessToken: AccessToken) {
+        Log.d(TAG, "handleFbLogn: $accessToken")
+        credential = FacebookAuthProvider.getCredential(accessToken.token)
+        FirebaseAuth.getInstance().signInWithCredential(credential!!)
+                .addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        Log.d(TAG, "FirebaseSignInWithCredential success")
+                    }
+                }
     }
 
     fun forgottenPassword(view: View) {
@@ -66,7 +124,35 @@ class SignInActivity : LifecycleActivity() {
             user: String = "",
             pass: String = ""
     ) {
-        Toast.makeText(this, "$user - $pass - $providerType", Toast.LENGTH_SHORT).show()
-        viewModel.doLogin(providerType, user, pass)
+        Log.d(TAG, "$user - $pass - $providerType")
+//        viewModel.doLogin(providerType, user, pass)
+        val permissions = getPermissionsForProvider(providerType)
+        when (providerType) {
+            SignInViewModel.SIGNIN_PROVIDER.FACEBOOK -> {
+                LoginManager.getInstance().logInWithReadPermissions(this, permissions)
+            }
+            SignInViewModel.SIGNIN_PROVIDER.GOOGLE -> {
+
+            }
+            SignInViewModel.SIGNIN_PROVIDER.TWITTER -> {
+
+            }
+            SignInViewModel.SIGNIN_PROVIDER.EMAIL -> {
+
+            }
+        }
+    }
+
+    private fun getPermissionsForProvider(provider: SignInViewModel.SIGNIN_PROVIDER): List<String> {
+        val permissions = mutableListOf<String>()
+        when (provider) {
+            SignInViewModel.SIGNIN_PROVIDER.FACEBOOK -> {
+                permissions.addAll(arrayOf("public_profile", "email"))
+            }
+            else -> {
+                // Do nothing by default
+            }
+        }
+        return permissions
     }
 }
