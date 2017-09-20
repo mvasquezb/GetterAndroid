236c578931b821cf96930970f8da144816a605df
Martín Vásquez <pedromvasquezb@gmail.com>
2017-09-20 12:45:56 -0500

    Add default map fragment

--- app/src/main/java/com/oligark/getter/view/ui/MainActivity.kt
+++ app/src/main/java/com/oligark/getter/view/ui/MainActivity.kt
@@ -3,10 +3,18 @@ package com.oligark.getter.view.ui
 import android.arch.lifecycle.LifecycleActivity
 import android.content.Intent
 import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.oligark.getter.R
 
-class MainActivity : LifecycleActivity() {
+class MainActivity : LifecycleActivity(), OnMapReadyCallback {
+
+    private var mMap: GoogleMap? = null
 
     override fun onCreate(savedInstanceState:Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,10 +25,21 @@ class MainActivity : LifecycleActivity() {
             startSignInActivity()
             return
         }
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
     }
 
     private fun startSignInActivity() {
         val signIn = Intent(this, SignInActivity::class.java)
         startActivity(signIn)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap!!.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap!!.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
