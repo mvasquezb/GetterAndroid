8408dcab3f0e0732d8f2379772b1c829eefebe74
Martín Vásquez <pedromvasquezb@gmail.com>
2017-09-25 00:21:23 -0500

    Add basic navigation drawer

--- app/src/main/java/com/oligark/getter/view/ui/MainActivity.kt
+++ app/src/main/java/com/oligark/getter/view/ui/MainActivity.kt
@@ -20,6 +20,12 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.mikepenz.materialdrawer.AccountHeader
+import com.mikepenz.materialdrawer.AccountHeaderBuilder
+import com.mikepenz.materialdrawer.Drawer
+import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem
 import com.oligark.getter.R
 import com.oligark.getter.databinding.ActivityMainBinding
 
@@ -31,6 +37,10 @@ class MainActivity : LifecycleActivity() {
     private lateinit var binding: ActivityMainBinding
     private var mLastQuery = ""
 
+    private lateinit var mUser: FirebaseUser
+    private lateinit var mDrawer: Drawer
+    private lateinit var navHeader: AccountHeader
+
     override fun onCreate(savedInstanceState:Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
@@ -40,12 +50,38 @@ class MainActivity : LifecycleActivity() {
             startSignInActivity()
             return
         }
+        mUser = auth.currentUser!!
+
         setupSearchbar()
+        setupDrawerMenu()
     }
 
     private fun startSignInActivity() {
         val signIn = Intent(this, SignInActivity::class.java)
         startActivity(signIn)
+    }
+
+    private fun setupDrawerMenu() {
+        // Add profile header
+        navHeader = AccountHeaderBuilder().withActivity(this)
+                .withHeaderBackground(R.drawable.login_round_border)
+                .addProfiles(
+                        ProfileDrawerItem().withName(mUser.displayName)
+                                .withEmail(mUser.email)
+                                .withIcon(R.mipmap.ic_launcher_round)
+                )
+                .withSelectionListEnabledForSingleProfile(false)
+                .build()
+
+        // Build basic drawer
+        mDrawer = DrawerBuilder().withActivity(this)
+                .withHeader(navHeader.view)
+                .inflateMenu(R.menu.menu_main)
+                .withSelectedItem(-1)
+                .build()
+
+        // Setup searchbar hamburger hook
+        binding.searchbar.attachNavigationDrawerToMenuButton(mDrawer.drawerLayout)
     }
 
     private fun setupSearchbar() {
@@ -89,6 +125,7 @@ class MainActivity : LifecycleActivity() {
                     override fun onFocusCleared() {
                         // Make last query remain in searchbar
                         binding.searchbar.setSearchBarTitle(mLastQuery)
+                        binding.searchbar.hideProgress()
                         Log.d(TAG, "onFocusCleared()")
                     }
 
