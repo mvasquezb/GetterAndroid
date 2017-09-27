bbd1f6d46a01cbdd9ec80a9eb79d5fe038a3d0b0
Martín Vásquez <pedromvasquezb@gmail.com>
2017-09-26 21:21:37 -0500

    Remove User FK in Business entity

--- app/src/main/java/com/oligark/getter/service/model/Business.kt
+++ app/src/main/java/com/oligark/getter/service/model/Business.kt
@@ -2,25 +2,19 @@ package com.oligark.getter.service.model
 
 import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
-import android.arch.persistence.room.ForeignKey
 import android.arch.persistence.room.PrimaryKey
 
 /**
- * Created by pmvb on 17-09-25.
+ * Business -> User relationship not necessary for this app
+ *
  */
-@Entity(foreignKeys = arrayOf(ForeignKey(
-        entity = User::class,
-        parentColumns = arrayOf("id"),
-        childColumns = arrayOf("owner_id")
-)))
+@Entity
 class Business(
         @PrimaryKey
         val id: Int,
         val name: String,
-        @ColumnInfo(name = "owner")
-        val owner_id: Int,
+        @ColumnInfo(name = "owner_id")
+        val ownerId: Int,
         @ColumnInfo(name = "logo_url")
         val logoUrl: String
-) {
-
-}
+)
