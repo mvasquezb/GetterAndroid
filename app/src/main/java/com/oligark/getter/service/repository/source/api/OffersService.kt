2cdfa9ead52678f274e15047fd7c17a506850763
Martín Vásquez <pedromvasquezb@gmail.com>
2017-09-29 01:09:27 -0500

    Modify OfferService methods to take nullable Boolean 'active'

--- app/src/main/java/com/oligark/getter/service/repository/source/api/OffersService.kt
+++ app/src/main/java/com/oligark/getter/service/repository/source/api/OffersService.kt
@@ -11,10 +11,10 @@ import retrofit2.http.Query
  */
 interface OffersService {
     @GET("api/offers/")
-    fun getOffers(@Query("active") active: Boolean = true): Call<List<Offer>>
+    fun getOffers(@Query("active") active: Boolean? = null): Call<List<Offer>>
 
     @GET("api/offers/count")
-    fun getOfferCount(@Query("active") active: Boolean = true): Call<Int>
+    fun getOfferCount(@Query("active") active: Boolean? = null): Call<Int>
 
     @GET("api/offers/{offer}")
     fun getOffer(@Path("offer") offerId: Int): Call<Offer>
@@ -22,18 +22,18 @@ interface OffersService {
     @GET("api/stores/{store}/offers")
     fun getStoreOffers(
             @Path("store") storeId: Int,
-            @Query("active") active: Boolean = true
+            @Query("active") active: Boolean? = null
     ): Call<List<Offer>>
 
     @GET("api/stores/{store}/offers/count")
     fun getStoreOfferCount(
             @Path("store") storeId: Int,
-            @Query("active") active: Boolean = true
+            @Query("active") active: Boolean? = null
     ): Call<Int>
 
     @GET("api/stores/{store}/offers/{offer}")
     fun getStoreOffer(
             @Path("store") storeId: Int,
-            @Query("active") active: Boolean = true
+            @Query("active") active: Boolean? = null
     ): Call<Offer>
 }
