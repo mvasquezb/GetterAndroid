36dcd335dccba83084d7977768d65b342ea76682
Martín Vásquez <pedromvasquezb@gmail.com>
2017-09-29 01:30:04 -0500

    Add OfferJsonAdapter. Update CustomMoshi object

--- app/src/main/java/com/oligark/getter/service/repository/source/remote/CustomMoshi.kt
+++ app/src/main/java/com/oligark/getter/service/repository/source/remote/CustomMoshi.kt
@@ -1,12 +1,17 @@
 package com.oligark.getter.service.repository.source.remote
 
+import com.squareup.moshi.KotlinJsonAdapterFactory
 import com.squareup.moshi.Moshi
+import com.squareup.moshi.Rfc3339DateJsonAdapter
 
 /**
  * Should be injected singleton
  */
 object CustomMoshi {
     val INSTANCE = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
             .add(StoreJsonAdapter())
+            .add(OfferJsonAdapter())
+            .add(Rfc3339DateJsonAdapter())
             .build()
 }
