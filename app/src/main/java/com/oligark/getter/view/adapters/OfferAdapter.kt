306cff24a516705e69001f842e3f910ca4c17a57
Martín Vásquez <pedromvasquezb@gmail.com>
2017-10-01 08:39:22 -0500

    Add offer update method with DiffUtil in OfferAdapter

--- app/src/main/java/com/oligark/getter/view/adapters/OfferAdapter.kt
+++ app/src/main/java/com/oligark/getter/view/adapters/OfferAdapter.kt
@@ -1,15 +1,14 @@
 package com.oligark.getter.view.adapters
 
+import android.support.v7.util.DiffUtil
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import com.oligark.getter.R
 import com.oligark.getter.service.model.Offer
-import com.squareup.picasso.Picasso
 import java.text.SimpleDateFormat
 
 /**
@@ -20,11 +19,15 @@ class OfferAdapter(
         private var offerSelectCallback: OfferAdapter.OnOfferSelectCallback
 ) : RecyclerView.Adapter<OfferAdapter.ViewHolder>() {
 
+    companion object {
+        val TAG = OfferAdapter::class.java.simpleName
+    }
+
     interface OnOfferSelectCallback {
         fun onOfferSelected(offer: Offer)
     }
 
-    private val simpleFormat = SimpleDateFormat("dd-MMM-yyyy HH:mm")
+    private val simpleFormat = SimpleDateFormat("dd/MM/yyyy HH:mm")
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
         return ViewHolder(
@@ -39,11 +42,37 @@ class OfferAdapter(
 
     override fun getItemCount(): Int = offers.size
 
+    fun setOfferList(offerList: List<Offer>) {
+        if (offers.isEmpty()) {
+            offers = offerList
+            notifyItemRangeInserted(0, offerList.size)
+            return
+        }
+        val diffResult = DiffUtil.calculateDiff(object : DiffUtil.Callback() {
+            override fun areItemsTheSame(
+                    oldItemPosition: Int,
+                    newItemPosition: Int
+            ): Boolean = offers[oldItemPosition].id == offerList[newItemPosition].id
+
+            override fun getOldListSize(): Int = offers.size
+
+            override fun getNewListSize(): Int = offerList.size
+
+            override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+                val oldOffer = offers[oldItemPosition]
+                val newOffer = offerList[newItemPosition]
+                return oldOffer.hasSameContent(newOffer)
+            }
+        })
+        offers = offerList
+        diffResult.dispatchUpdatesTo(this)
+    }
+
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val productImage: ImageView = itemView.findViewById(R.id.product_image)
         val offerDescription: TextView = itemView.findViewById(R.id.offer_description)
         val offerDate: TextView = itemView.findViewById(R.id.offer_date)
-        val offerGetButton: ImageButton = itemView.findViewById(R.id.offer_get_button)
+        val offerGetButton: ImageView = itemView.findViewById(R.id.offer_get_button)
 
         fun bind(offer: Offer) {
 //            Picasso.with(itemView.context)
