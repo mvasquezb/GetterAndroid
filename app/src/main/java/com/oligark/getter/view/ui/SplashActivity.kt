579b1c02558903598304eb3d14014416ba71e4e8
Martín Vásquez <pedromvasquezb@gmail.com>
2017-10-01 15:33:05 -0500

    Add basic splash screen

---
+++ app/src/main/java/com/oligark/getter/view/ui/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.oligark.getter.view.ui
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
