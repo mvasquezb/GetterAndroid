e63300e5c3e25cdf58e63bda0511da7ad679e038
Martín Vásquez <pedromvasquezb@gmail.com>
2017-10-08 02:07:11 -0500

    Add EmailCredentialsState resource

---
+++ app/src/main/java/com/oligark/getter/viewmodel/resources/EmailCredentialsState.kt
@@ -0,0 +1,14 @@
+package com.oligark.getter.viewmodel.resources
+
+/**
+ * Created by pmvb on 17-10-08.
+ */
+enum class EmailCredentialsState {
+    VALID,
+    EMPTY_EMAIL,
+    EMPTY_PASSWORD,
+    INVALID_EMAIL,
+    INVALID_PASSWORD,
+    VALID_EMAIL,
+    VALID_PASSWORD,
+}
